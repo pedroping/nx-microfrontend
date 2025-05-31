e00765e3a1e9743766c7763b818d7635bc3fbbad
pedroping <pedro.dias@moneyprestador.com.br>
2025-05-31 18:50:42 -0300

    Fix: remove build errors and make prod environment works

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { Route } from '@angular/router';
 export const appRoutes: Route[] = [
   {
     path: 'mfe1',
-    loadChildren: () => import('mfe1/Routes').then((m) => m!.remoteRoutes),
+    loadChildren: () => import('mfe1/Routes').then((m) => m.remoteRoutes),
   },
   {
     path: '',
